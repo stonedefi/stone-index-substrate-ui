f05e3e6944d060b885f45241f2de9aa5fe1bd33b
Raphael Yu <raphael@rockx.com>
2021-09-15 22:42:42 +0800

    Support the type `IndexId`.

--- src/__tests__/types.js
+++ src/__tests__/types.js
@@ -2,12 +2,13 @@ import types from '../config/types.json';
 
 describe('Types Test Suite', () => {
   it('types has at lease three value', () => {
-    expect(Object.keys(types).length).toBeGreaterThanOrEqual(2);
+    expect(Object.keys(types).length).toEqual(3);
   });
 
   it('the common has StoneIndexComponent,StoneIndex on it', () => {
     expect(Object.keys(types)).toContain('StoneIndexComponent');
     expect(Object.keys(types)).toContain('StoneIndex');
+    expect(Object.keys(types)).toContain('IndexId');
   });
 
   it('StoneIndexComponent and StoneIndex has at lease two value', () => {
